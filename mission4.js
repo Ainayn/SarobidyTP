a09469fd4fc6cda16dff5d540a59696f0a6c727c
Ny Aina <sarobidynyaina011202@gmail.com>
2022-11-29 14:46:49 +0100

    index29.11

---
+++ mission4.js
@@ -0,0 +1,19 @@
+function score(){
+    var juste, faute =0;
+    if(document.getElementById().checked == true){
+        x=x+1
+    }
+    else{
+        faute= faute+1;
+    }
+}
+
+function reint()
+	{
+		window.location.reload();
+	}
+
+function corriger()
+{
+    document.location.href="mission4corrige.html"
+}
